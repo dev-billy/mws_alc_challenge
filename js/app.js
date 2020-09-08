fcb636235459c6a62302c1b9e026559990714018
Billy <billypatrick767@gmail.com>
2020-09-08 12:20:11 +0300

    made the dom manipulation modular

--- js/app.js
+++ js/app.js
@@ -1,27 +1,20 @@
 const API_KEY = '9f18e84e6874fc2fb81e7bb28c932f88';
-let searchedLocation = 'New York'
+let searchedLocation = 'Accra'
 
 const openWeatherApi = `http://api.openweathermap.org/data/2.5/forecast?q=${searchedLocation}&appid=${API_KEY}`;
 
 const weatherLocation = document.getElementById('weather_location');
 
-const day0Temp = document.getElementById('day_0_temp');
-const day1Temp = document.getElementById('day_1_temp');
-const day2Temp = document.getElementById('day_2_temp');
-const day3Temp = document.getElementById('day_3_temp');
-const day4Temp = document.getElementById('day_4_temp');
 
-const day0Date = document.getElementById('day_0_date');
-const day1Day = document.getElementById('day_1_day');
-const day2Day = document.getElementById('day_2_day');
-const day3Day = document.getElementById('day_3_day');
-const day4Day = document.getElementById('day_4_day');
+const daysTemp = ['day_temp0','day_temp1','day_temp2','day_temp3','day_temp4'];
+const daysDate = ['date0','day1','day2','day3','day4'];
+const daysWeather = ['day_weather0','day_weather1','day_weather2','day_weather3','day_weather4']
 
-const day0Weather = document.getElementById('day_0_weather');
-const day1Weather = document.getElementById('day_1_weather');
-const day2Weather = document.getElementById('day_2_weather');
-const day3Weather = document.getElementById('day_3_weather');
-const day4Weather = document.getElementById('day_4_weather');
+const daysArray = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+const dayShortArr = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
+
+const monthsArray = ['January','February','March','May','June','July','August','September','October','November','December'];
+
 
 let promise = fetch(openWeatherApi)
                 .then(data =>{
@@ -30,25 +23,34 @@ let promise = fetch(openWeatherApi)
                     weatherLocation.innerHTML = results.city.name + ', ' + results.city.country;
 
                     let myRes = [];
+
                     const date = new Date();
-                    let currentDay = date.getDay();
+                    let currentDay = date.getUTCDay();
+
+                    document.getElementById(daysDate[0]).innerHTML = 
+                                                                    daysArray[currentDay] + ', ' + 
+                                                                    date.getDate() + ' ' + monthsArray[date.getMonth()] + ' ' 
+                                                                    + date.getFullYear();
 
                     let myList = results.list.forEach(item => {
-                        let apiDt = new Date(item.dt *1000);
-                        if(currentDay != apiDt.getDay()){
+                        let apiDay = new Date(item.dt *1000).getUTCDay();
+                        if(currentDay == apiDay){
                             myRes.push(item);   
-                            currentDay += 1;
+                            currentDay = currentDay + 1;
+                            apiDay = apiDay +1;
+                        }else{
+                            return
                         }
                   });
-                    day0Temp.innerHTML=Math.ceil(myRes[0].main.temp - 273.15) + ' °C';
-                    day1Temp.innerHTML=Math.ceil(myRes[1].main.temp - 273.15) + ' °C';
-                    day2Temp.innerHTML=Math.ceil(myRes[2].main.temp - 273.15) + ' °C';
-                    day3Temp.innerHTML=Math.ceil(myRes[3].main.temp - 273.15) + ' °C';
-                    day4Temp.innerHTML=Math.ceil(myRes[4].main.temp - 273.15) + ' °C';
+                
 
-                    day0Weather.innerHTML=myRes[0].weather[0].main;
-                    day1Weather.innerHTML=myRes[1].weather[0].main;
-                    day2Weather.innerHTML=myRes[2].weather[0].main;
-                    day3Weather.innerHTML=myRes[3].weather[0].main;
-                    day4Weather.innerHTML=myRes[4].weather[0].main;
-                })
+                for(let i =0; i<5; i++){
+                    document.getElementById(daysTemp[i]).innerHTML = Math.ceil(myRes[i].main.temp - 273.15) + ' °C'; 
+                    document.getElementById(daysWeather[i]).innerHTML = myRes[i].weather[0].main;
+                    let resDay = new Date(myRes[i].dt * 1000).getUTCDay();
+                    
+                    if(i != 0 && i <=5){
+                        document.getElementById(daysDate[i]).innerHTML = dayShortArr[resDay];
+                    }
+                }                 
+            })
