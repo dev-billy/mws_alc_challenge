b7ffe04a2a0c437af3fa0d6750ebe873f0cc75e9
Billy <billypatrick767@gmail.com>
2020-09-11 11:34:21 +0300

    refactor location and changed some styles

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const onFail = document.getElementById('on-fail');
 
 
 dynamicContent.style.display = 'none';
+loader.style.display = 'none';
 
 searchInput.addEventListener('keyup',(e)=>{
     if(e.keyCode === 13){
@@ -34,28 +35,17 @@ searchBtn.addEventListener('click',()=>{
 function runUrl(userSearch) {
     const API_KEY = '9f18e84e6874fc2fb81e7bb28c932f88';
     let searchedLocation = userSearch;
-   
-
-    const openWeatherApi = `http://api.openweathermap.org/data/2.5/forecast?q=${searchedLocation}&appid=${API_KEY}`;
-
-    const weatherLocation = document.getElementById('weather_location');
-
-
-    const daysTemp = ['day_temp0','day_temp1','day_temp2','day_temp3','day_temp4'];
-    const daysDate = ['date0','day1','day2','day3','day4'];
-    const daysWeather = ['day_weather0','day_weather1','day_weather2','day_weather3','day_weather4']
-
+    const openWeatherApi = `https://api.openweathermap.org/data/2.5/forecast?q=${searchedLocation}&appid=${API_KEY}`;
+    const weatherLocation = document.getElementById('weather_location'); 
+    const daysDate = ['date0'];    
     const daysArray = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-    const dayShortArr = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
-
     const monthsArray = ['January','February','March','May','June','July','August','September','October','November','December'];
-
-    const iconArr = ['weather_icon0','weather_icon1','weather_icon2','weather_icon3','weather_icon4']; 
-
+     
     let promise = fetch(openWeatherApi)
                     .then(response =>{
                         if(!response.ok){
                             loader.style.display = 'block';
+                            loader.style.background = '#f1f1f1';
                             beforeSearch.style.display ='none';
                             onSearch.style.display = 'none';
                             onFail.style.display = 'block';
@@ -71,14 +61,10 @@ function runUrl(userSearch) {
                         onSearch.style.display ='none';
                         onFail.style.display = 'none';
                         dynamicContent.style.display = 'block';
-                        
                         weatherLocation.innerHTML = results.city.name + ', ' + results.city.country;
-
-                        let myRes = [];
-                        
+                        let myRes = [];                   
                         const date = new Date();
                         let currentDay = date.getUTCDay();
-
                         document.getElementById(daysDate[0]).innerHTML = 
                                                                         daysArray[currentDay] + ', ' + 
                                                                         date.getDate() + ' ' + monthsArray[date.getMonth()] + ' ' 
@@ -96,22 +82,40 @@ function runUrl(userSearch) {
        
                     });
 
-                    
-
-                    for(let i =0; i<5; i++){
-                        document.getElementById(daysTemp[i]).innerHTML = Math.ceil(myRes[i].main.temp - 273.15) + ' °C'; 
-                        document.getElementById(daysWeather[i]).innerHTML = myRes[i].weather[0].main;
-                        document.getElementById(iconArr[i]).src = `http://openweathermap.org/img/wn/${myRes[i].weather[0].icon}@2x.png`;
-                        let resDay = new Date(myRes[i].dt * 1000).getUTCDay();
-                        let counter = i;
-                        if(counter != 0 && i <=5){
-                            document.getElementById(daysDate[counter]).innerHTML = dayShortArr[resDay];
-                        }
-                    }                 
+                    displayData(myRes)              
                 }).catch(error =>{
-                    dynamicContent.style.display = 'none';
-                    
-                    
+                    dynamicContent.style.display = 'none';    
                 })
 }
     
+
+function displayData(myRes){
+    const daysTemp = ['day_temp0','day_temp1','day_temp2','day_temp3','day_temp4'];
+    const daysWeather = ['day_weather0','day_weather1','day_weather2','day_weather3','day_weather4'];
+    const dayShortArr = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
+    const iconArr = ['weather_icon0','weather_icon1','weather_icon2','weather_icon3','weather_icon4'];
+    const daysDate = ['date0','day1','day2','day3','day4'];    
+
+    for(let i =0; i<5; i++){
+        document.getElementById(daysTemp[i]).innerHTML = Math.ceil(myRes[i].main.temp - 273.15) + ' °C'; 
+        document.getElementById(daysWeather[i]).innerHTML = myRes[i].weather[0].main;
+        document.getElementById(iconArr[i]).src = `https://openweathermap.org/img/wn/${myRes[i].weather[0].icon}@2x.png`;
+        let resDay = new Date(myRes[i].dt * 1000).getUTCDay();
+        let counter = i;
+        if(counter != 0 && i <=5){
+            document.getElementById(daysDate[counter]).innerHTML = dayShortArr[resDay];
+        }
+    }   
+}
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('./js/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
